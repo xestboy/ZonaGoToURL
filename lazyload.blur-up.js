a879207ea497ab7291bf003dd5c01490de63838f
Gosha <43237783+xestboy@users.noreply.github.com>
2021-03-15 09:26:21 +0300

    Update lazyload.blur-up.js

--- lazyload.blur-up.js
+++ lazyload.blur-up.js
@@ -1,1 +1,1 @@
-var media_loaded=function(a){a.className+=" shown",a.classList.remove("blur-up")};deferimg("img.lazyload",1180,"lazied",media_loaded),deferiframe("iframe[data-src],[data-style]");
+var media_loaded=function(a){a.className+=" shown",a.classList.remove("blur-up")};deferimg("img.lazyload",80,"lazied",media_loaded),deferiframe("iframe[data-src],[data-style]");
